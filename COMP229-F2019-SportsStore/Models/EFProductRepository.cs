d01f81c0a0e47303815ef338b9cf8fab249dc8d2
EderCard <edersoncard@gmail.com>
2019-10-03 23:02:16 -0400

    Add EntityFramework and DbContext

---
+++ COMP229-F2019-SportsStore/Models/EFProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace COMP229_F2019_SportsStore.Models
+{
+    public class EFProductRepository : IProductRepository
+    {
+        private ApplicationDbContext context;
+
+        public EFProductRepository(ApplicationDbContext ctx)
+        {
+            this.context = ctx;
+        }
+        public IQueryable<Product> Products => context.Products;
+    }
+}
